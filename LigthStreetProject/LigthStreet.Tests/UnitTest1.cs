add93ae1ca7f5dde6436c154fd5e8b361fb3ca0e
Mishka Sorochinskiy <mishash20161506@gmail.com>
2020-04-10 22:11:00 +0300

    added all needed projects

---
+++ LigthStreetProject/LigthStreet.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace LigthStreet.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
