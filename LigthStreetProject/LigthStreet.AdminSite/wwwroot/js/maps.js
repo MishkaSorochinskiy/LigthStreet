e93c769bef4638578cfd1e648e2152f6df5297cc
nazariizahoroda <nazarii.zahoroda.kn.2016@lpnu.ua>
2020-04-12 16:58:26 +0300

    Added route to map

--- LigthStreetProject/LigthStreet.AdminSite/wwwroot/js/maps.js
+++ LigthStreetProject/LigthStreet.AdminSite/wwwroot/js/maps.js
@@ -1,50 +1,30 @@
-﻿var api_key = "AIzaSyBN2flaovRh2XfVjhrADu7F7r-mxZSJ6iA";
+﻿var directionsService = null;
+var directionsRenderer = null;
+
 function initMap() {
-    // The location of Uluru
-    var uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: uluru });
-    // The marker, positioned at Uluru
-    //var image = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxFoh469eOsZQkuPOLpZn3R6yyIExkZCxOxf4ywfeY3v330EwP3Q";
-    //var marker = new google.maps.Marker({
-    //    position: uluru,
-    //    map: map,
-    //    icon: image
-    //});
-    //var marker = new google.maps.Marker({ position: uluru, icon:"", map: map });
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+    var mapOptions = {
+        zoom: 7,
+        center: chicago
+    }
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsRenderer.setMap(map);
 }
 
-function getRouteValues() {
-    let origin = document.getElementById("origin").value;
-    let destination = document.getElementById("destination").value;
-  
-    let url = `https://maps.googleapis.com/maps/api/directions/json?` +
-        `origin=${origin}&destination=${destination}&key=${api_key}`;
+function calcRoute() {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: 'DRIVING'
+    };
+    directionsService.route(request, function (result, status) {
+        if (status == 'OK') {
+            directionsRenderer.setDirections(result);
 
-    //$.ajax({
-    //    type: "Get",
-    //    url: url,
-    //    dataType: 'json',
-    //    success: function () {
-    //        console.log("Hello");
-    //    },
-    //    failure: function (Error, errr, error) {
-    //        var a = error;
-    //    }
-    //})
-
-    $.ajax({
-        url: "https://maps.googleapis.com/maps/api/directions/json",
-        type: "GET",
-        data: {
-            origins: origin,
-            destination: value,
-            key: api_key
-        },
-        success: function (data) {
-            console.log(data);
         }
     });
 }
-
