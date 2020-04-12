1909790c26e727541bc563027ec0243bbed31696
Mishka Sorochinskiy <mishash20161506@gmail.com>
2020-04-12 17:26:24 +0300

    added multiple routes

--- LigthStreetProject/LigthStreet.AdminSite/wwwroot/js/maps.js
+++ LigthStreetProject/LigthStreet.AdminSite/wwwroot/js/maps.js
@@ -1,6 +1,6 @@
 ﻿var directionsService = null;
 var directionsRenderer = null;
-
+var map = null;
 function initMap() {
     directionsService = new google.maps.DirectionsService();
     directionsRenderer = new google.maps.DirectionsRenderer();
@@ -9,7 +9,7 @@ function initMap() {
         zoom: 7,
         center: chicago
     }
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
     directionsRenderer.setMap(map);
 }
 
@@ -19,12 +19,34 @@ function calcRoute() {
     var request = {
         origin: start,
         destination: end,
-        travelMode: 'DRIVING'
+        travelMode: 'DRIVING',
+        provideRouteAlternatives: true
     };
     directionsService.route(request, function (result, status) {
         if (status == 'OK') {
-            directionsRenderer.setDirections(result);
-
+            //directionsRenderer.setDirections(result);
+            for (var i = 0, len = result.routes.length; i < len; i++) {
+                var polylineOptionsActual = new google.maps.Polyline({
+                    strokeColor: getRandomColor(),
+                    strokeOpacity: 1.0,
+                    strokeWeight: 6
+                });
+                new google.maps.DirectionsRenderer({
+                    map: map,
+                    directions: result,
+                    routeIndex: i,
+                    polylineOptions: polylineOptionsActual              
+                });
+            }
         }
     });
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
